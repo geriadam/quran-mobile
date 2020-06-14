26008f1858ce3e0f4d0eed322d207caef6c8dd20
geri <geriadam1@gmail.com>
2020-06-14 18:14:44 +0700

    register components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { CardSurahList } from "./CardSurahList";
 export { Header } from "./Header";
+export { HeaderDetail } from "./HeaderDetail";
+export { CardAyat } from "./CardAyat";
+export { Loader } from "./Loader";
