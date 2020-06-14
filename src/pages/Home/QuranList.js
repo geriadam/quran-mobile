231022da2a61bd24da04261b6e30427cb67dd191
geri <geriadam1@gmail.com>
2020-06-14 18:14:31 +0700

    QuranList Pages: add loader when state loading

--- src/pages/Home/QuranList.js
+++ src/pages/Home/QuranList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, FlatList, RefreshControl } from 'react-native';
 import { Divider } from 'react-native-paper'
 import styles from './styles';
-import { CardSurahList, Header } from "../../components/index";
+import { CardSurahList, Loader, Header } from "../../components/index";
 
 import { fetchingQuran } from '../../actions/QuranListAction';
 import { connect } from 'react-redux';
@@ -50,25 +50,27 @@ class QuranList extends Component {
     }
 
     render() {
-        return (
-            <View style={styles.container}>
-                <Header title="Al Quran Mobile" />
-                <View style={styles.contentContainer}>
-                    <FlatList
-                        data={this.props.quranList.data}
-                        renderItem={this.renderItem}
-                        keyExtractor={item => `${item.id}`}
-                        ItemSeparatorComponent={Divider}
-                        refreshControl={
-                            <RefreshControl
-                                refreshing={this.state.refreshing}
-                                onRefresh={this._onRefresh}
+        return this.props.quranList.loading 
+                ? ( <Loader loading={this.props.quranList.loading} /> )
+                : (
+                    <View style={styles.container}>
+                        <Header title="Al Quran Mobile" />
+                        <View style={styles.contentContainer}>
+                            <FlatList
+                                data={this.props.quranList.data}
+                                renderItem={this.renderItem}
+                                keyExtractor={item => `${item.id}`}
+                                ItemSeparatorComponent={Divider}
+                                refreshControl={
+                                    <RefreshControl
+                                        refreshing={this.state.refreshing}
+                                        onRefresh={this._onRefresh}
+                                    />
+                                }
                             />
-                        }
-                    />
-                </View>
-            </View>
-        );
+                        </View>
+                    </View>
+                );
     }
 }
 
